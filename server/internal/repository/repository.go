f33fbea7113acaad1cef5c220e1228b20f5bf0cb
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2025-07-24 12:52:43 -0400

    feat: create bottles table

--- server/internal/repository/repository.go
+++ server/internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,7 +12,7 @@ type Repository struct {
 }
 
 func New() *Repository {
-	db, err := sql.Open("sqlite3", "../database/app.db")
+	db, err := sql.Open("sqlite3", "./internal/database/app.db")
 	if err != nil {
 		panic(err)
 	}
@@ -21,4 +22,19 @@ func New() *Repository {
 
 func (r *Repository) CloseDB() {
 	r.db.Close()
+}
+
+func (r *Repository) CreateBottlesTable() error {
+	if _, err := r.db.Exec(`
+		CREATE TABLE IF NOT EXISTS bottles (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+	`); err != nil {
+		return fmt.Errorf("CreateBottlesTable(): %v", err)
+	}
+
+	return nil
 }
--- server/main.go
+++ server/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/nguyenjessev/liquor-locker/internal/repository"
 )
 
 func main() {
-	fmt.Println("Hello, Liquor Locker!")
+	repo := repository.New()
+	defer repo.CloseDB()
+
+	err := repo.CreateBottlesTable()
+	if err != nil {
+		fmt.Println("Error creating bottles table:", err)
+		return
+	}
 }
