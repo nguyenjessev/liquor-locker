a6e6290712e535d89880a1a16e5b26bb92b33388
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2025-08-26 13:53:29 -0400

    feat: add price to fresh card

--- client/src/components/FreshCard.tsx
+++ client/src/components/FreshCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import type { Fresh } from "@/types/fresh";
 import { format } from "date-fns";
 
@@ -18,38 +18,40 @@ export function FreshCard({ fresh, onEdit }: FreshCardProps) {
 
 	return (
 		<Card
-			className="hover:shadow-md transition-shadow relative cursor-pointer"
+			className="hover:shadow-md transition-shadow cursor-pointer gap-1"
 			onClick={() => onEdit(fresh)}
 		>
 			<CardHeader>
-				<div className="flex justify-between items-start gap-2">
-					<div>
-						<div className="flex items-center gap-2">
-							<div className="flex flex-wrap items-center gap-2">
-								<CardTitle className="text-lg break-words">
-									{fresh.name}
-								</CardTitle>
-							</div>
-						</div>
-						{fresh.purchase_date && (
-							<div className="flex items-center gap-2 mt-2">
-								<span className="text-sm font-medium">Purchased:</span>
-								<span className="text-sm text-muted-foreground">
-									{formatDate(fresh.purchase_date)}
-								</span>
-							</div>
-						)}
-						{fresh.prepared_date && (
-							<div className="flex items-center gap-2 mt-2">
-								<span className="text-sm font-medium">Prepared:</span>
-								<span className="text-sm text-muted-foreground">
-									{formatDate(fresh.prepared_date)}
-								</span>
-							</div>
-						)}
-					</div>
-				</div>
+				<CardTitle className="text-lg break-words">{fresh.name}</CardTitle>
 			</CardHeader>
+			<CardContent>
+				{fresh.price && (
+					<div className="text-sm">
+						Price:{" "}
+						<span className="text-sm text-muted-foreground">
+							{fresh.price.toFixed(2)}
+						</span>
+					</div>
+				)}
+
+				{fresh.purchase_date && (
+					<div className="text-sm">
+						Purchased:{" "}
+						<span className="text-sm text-muted-foreground">
+							{formatDate(fresh.purchase_date)}
+						</span>
+					</div>
+				)}
+
+				{fresh.prepared_date && (
+					<div className="text-sm">
+						Prepared:{" "}
+						<span className="text-sm text-muted-foreground">
+							{formatDate(fresh.prepared_date)}
+						</span>
+					</div>
+				)}
+			</CardContent>
 		</Card>
 	);
 }
